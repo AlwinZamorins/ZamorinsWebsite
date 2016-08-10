0922d897e71fd3e8239e6385d33e3d3ac0f099bb
AlwinZamorins <alwin@zamorinstech.com>
2016-08-10 11:55:24 +0530

    Started loading screen

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -2,17 +2,17 @@ var app = angular.module('MyApp', ["ngRoute"]);
 
 //code for linking content from different HTML pages
 app.config(function($routeProvider) {
-        $routeProvider
+        /*$routeProvider
             .when("/login", {
                 templateUrl: "../content/_login.html",
                 controller: "loginController"
             })
             .otherwise({
-                redirectTo: '../content/_barInfo.html'
-            });
+                redirectTo: 'index.html'
+            });*/
     })
 
 //Controller for GLOBAL
 app.controller('mainController', function($scope) {
-   alert();
+    
 });
