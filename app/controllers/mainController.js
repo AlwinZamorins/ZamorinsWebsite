87fa3cc047d3206574aabd1cfcf4608ff81b5877
AlwinZamorins <alwin@zamorinstech.com>
2016-08-10 17:43:48 +0530

    Navigation bar upload

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -4,23 +4,70 @@ var app = angular.module('MyApp', ['ngRoute']);
 app.config(function($routeProvider) {
     $routeProvider
             .when("/", {
-                templateUrl: "veiws/home.html",
+                templateUrl: "views/home.html",
                 controller: "homeController"
+            })
+            .when("/home", {
+                templateUrl: "views/home.html",
+                controller: "homeController"
+            })
+            .when("/about", {
+                templateUrl: "views/about.html",
+                controller: "aboutController"
+            })
+            .when("/works", {
+                templateUrl: "views/works.html",
+                controller: "worksController"
+            })
+            .when("/team", {
+                templateUrl: "views/team.html",
+                controller: "teamController"
+            })
+            .when("/contact", {
+                templateUrl: "views/contact.html",
+                controller: "contactController"
             })
     })
 //Controller for GLOBAL
+
 app.controller('mainController', function($scope) {
-    /*Loading Start*/
     window.addEventListener("load",function(){
-        var loader_div = angular.element(document.querySelector('#loader_div'));
-        var main_content = angular.element(document.querySelector('#main_content'));
-        loader_div.addClass('hide');
-        main_content.removeClass('hide');
+        $("#loader_div").addClass('hide');
+        $("#main_content").removeClass('hide');
     });
-     /*Loading Finish*/
 });
-
-
 app.controller('homeController', function($scope) {
-    alert('home');
+    $("#nav_home").addClass('selected_nav');
+    $("#nav_about").removeClass('selected_nav');
+    $("#nav_team").removeClass('selected_nav');
+    $("#nav_works").removeClass('selected_nav');
+    $("#nav_contact").removeClass('selected_nav');
+});
+app.controller('aboutController', function($scope) {
+    $("#nav_home").removeClass('selected_nav');
+    $("#nav_about").addClass('selected_nav');
+    $("#nav_team").removeClass('selected_nav');
+    $("#nav_works").removeClass('selected_nav');
+    $("#nav_contact").removeClass('selected_nav');
+});
+app.controller('teamController', function($scope) {
+    $("#nav_home").removeClass('selected_nav');
+    $("#nav_about").removeClass('selected_nav');
+    $("#nav_team").addClass('selected_nav');
+    $("#nav_works").removeClass('selected_nav');
+    $("#nav_contact").removeClass('selected_nav');
+});
+app.controller('worksController', function($scope) {
+    $("#nav_home").removeClass('selected_nav');
+    $("#nav_about").removeClass('selected_nav');
+    $("#nav_team").removeClass('selected_nav');
+    $("#nav_works").addClass('selected_nav');
+    $("#nav_contact").removeClass('selected_nav');
+});
+app.controller('contactController', function($scope) {
+    $("#nav_home").removeClass('selected_nav');
+    $("#nav_about").removeClass('selected_nav');
+    $("#nav_team").removeClass('selected_nav');
+    $("#nav_works").removeClass('selected_nav');
+    $("#nav_contact").addClass('selected_nav');
 });
